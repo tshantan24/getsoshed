fb6325f31bbcab2fb0d7114f77a1d92dcaf9ad46
Shantan Thota <36533123+tshantan24@users.noreply.github.com>
2023-03-16 06:06:00 +0530

    Made some base level changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+//Modules
+import { SharedModule } from './shared/shared.module';
+
+//Components
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
