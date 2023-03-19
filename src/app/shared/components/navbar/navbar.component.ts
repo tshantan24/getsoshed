6ec7b616a48b6eaea126cb2235c6cb51780803e6
Shantan Thota <36533123+tshantan24@users.noreply.github.com>
2023-03-19 19:36:00 +0530

    Added Navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sosh-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.sass']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  //Logos
+  logoUrl!: string;
+  logoTitle!: string;
+
+  constructor(){
+    this.logoTitle = 'Soshed';
+    this.logoUrl = './assets/images/brand/soshed-main-dark.png'
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
