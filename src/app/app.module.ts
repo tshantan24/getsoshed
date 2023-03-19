2eda47c730ac4928dd15f9ca74eecab12bec98e5
Shantan Thota <36533123+tshantan24@users.noreply.github.com>
2023-03-19 19:39:34 +0530

    Added Contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sosh-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.sass']
+})
+export class ContactComponent {
+
+}
